b2bb055cb41a33d7e898dc039b7db10fd59eb1be
Teo Morlans <teomorlans@gmail.com>
2024-02-27 16:10:10 +0100

    def controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,40 @@
 class BookingsController < ApplicationController
+  before_action :set_planet, only: [:new, :create]
+
   def index
+    @bookings = Booking.all
   end
 
   def show
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.planet = @planet
+    @booking.user = current_user
+      redirect_to planet_path(@planet), notice: 'Your reservation has been successfully registered.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_url, notice: 'The reservation has been canceled.'
   end
 
-  def delete
+  private
+
+  def set_planet
+    @planet = Planet.find(params[:planet_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
