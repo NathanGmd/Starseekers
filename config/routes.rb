1f36210050ff988c5993f19ac35151265e2a5df1
Nathan Germond <germond.nathan@gmail.com>
2024-02-27 12:41:46 +0100

    routes, migrate, devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'bookings/index'
   get 'bookings/show'
   get 'bookings/new'
@@ -7,12 +8,13 @@ Rails.application.routes.draw do
   get 'bookings/delete'
   get 'planets/index'
   get 'planets/show'
-  root to: "pages#home"
+  root to: "planets#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :planets
 
   # Defines the root path route ("/")
   # root "posts#index"
