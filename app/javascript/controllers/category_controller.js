3716770ea737df25e53303f67286b2f9f53ae520
Nathan Germond <germond.nathan@gmail.com>
2024-02-28 15:26:17 +0100

    maj

---
+++ app/javascript/controllers/category_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="category"
+export default class extends Controller {
+  static targets = ["categoryName"];
+
+  connect() {
+    console.log("hello form category controller")
+    console.log(this.categoryNameTarget.textContent);
+  }
+}
